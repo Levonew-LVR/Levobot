282ed467203c28bc425743f8e1eecf60ea42584a
Levo New <loboboy005@gmail.com>
2026-02-04 09:22:43 -0500

    1.2
    Test 1.5

--- Bot.py
+++ Bot.py
@@ -163,7 +163,7 @@ async def comand_forward(client, message: Message):
         text_report = f"""
 👤 Reporte del Usarios
 Usario: @{user.username or user.first_name}
-Usario reportado {user_repor.mention} { 'ID: ' user_repor.id}
+Usario reportado {user_repor.mention} ID:{user_repor.id}
 chat: {message.chat.title or 'En pv'} {chat.id}
 fecha: {message.date}
 Mensaje reportado {message.reply_to_message.id}
